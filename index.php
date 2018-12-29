668d4eb12501b02777c0a663702d9e85c2c8ad8d
FilipeQuina <filipe.pacheco00@gmail.com>
2018-12-29 14:25:12 -0200

    ajuste na arquitetura

--- index.php
+++ index.php
@@ -3,10 +3,19 @@
 require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 $app = new Slim();
 
 $app->config('debug', true);
 
+
+$app->get('/ecommerce/admin', function() {
+    
+	$page = new PageAdmin();
+	echo("as");
+	$page->setTpl("index");
+
+});
 $app->get('/', function() {
     
 	$page = new Page();
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -9,22 +9,18 @@ class Page
     private $defaults = [
         "data" => []
     ];
-    public function __construct($opts=array())
+
+    public function __construct($opts=array(),$tpl_dir = "/views/")
     {
         $this->options = array_merge($this->defaults, $opts);
         $config = array(
-            "tpl_dir" => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/vendor/views/",
-            "cache_dir" => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/vendor/views-cache/",
-            "debug" => true // set to false to improve the speed
+            "tpl_dir" => $_SERVER["DOCUMENT_ROOT"]."/ecommerce".$tpl_dir,
+            "cache_dir" => $_SERVER["DOCUMENT_ROOT"]."/ecommerce/views-cache/",
+            "debug" => false // set to false to improve the speed
         );
- 
+
         Tpl::configure($config);
-        
-
-        $this->tpl = new Tpl;
-
-
-        
+        $this->tpl = new Tpl;        
         $this->setData($this->options["data"]);
         $this->tpl->draw("header");
     }
@@ -33,13 +29,13 @@ class Page
         foreach ($data as $key => $value){
             $this->tpl->assign($key,$value);
         }
-
     }
 
     public function setTpl($name,$data = array(), $returnHTML = false){
         $this->setData($data);
         return $this->tpl->draw($name,$returnHTML);
     }
+
     public function __destruct()
     {
         $this->tpl->draw("footer");
